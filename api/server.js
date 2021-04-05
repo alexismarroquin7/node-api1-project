b80201cc5d97582a78536bf8fb966a2130288c32
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-05 13:23:35 -0700

    edit user

--- api/server.js
+++ api/server.js
@@ -51,4 +51,25 @@ server.post(`/api/users`, async (req, res) => {
 });
 
 
+// [PUT] - Edit user by id
+server.put(`/api/users/:id`, async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    try {
+        if(!changes.name || !changes.bio){
+            res.status(400).json({ message: "Please provide name and bio for the user" });
+        } else {
+            const updatedUser = await User.update(id, changes);
+            if(!updatedUser){
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            } else {
+                res.status(201).json(updatedUser);
+            }
+        }
+    } catch {
+        res.status(500).json({ message: "The user information could not be modified" });
+    }
+});
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
