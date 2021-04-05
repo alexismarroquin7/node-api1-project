cb56245bdd86ef8241aef641eb0c5530a13b5df8
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-05 13:22:26 -0700

    get user by id

--- api/server.js
+++ api/server.js
@@ -6,4 +6,20 @@ server.use(express.json());
 
 const User = require('./users/model.js');
 
+
+// [GET] user by id
+server.get('/api/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        if(!user){
+            res.status(404).json({ message: "The user with the specified ID does not exist" });
+        } else {
+            res.status(200).json(user);
+        }
+    } catch {
+        res.status(500).json({ message: 'The user information could not be retrieved' });
+    }
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
